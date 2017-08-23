acd9cada052cc57186cf38bf953b54f0dd473bca
earthmant <trammell@cloudify.co>
2017-08-23 11:41:41 +0300

    adding local input

--- azure-custom-script.sh
+++ azure-custom-script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -xe
 
 PUBLIC_IP=$1
+PRIVATE_IP=$1
 
 function run_until_success {
     COUNTER=0
@@ -19,7 +20,7 @@ cat /home/cfyuser/.ssh/key.pem.pub >> /home/cfyuser/.ssh/authorized_keys
 sudo rpm -i http://repository.cloudifysource.org/cloudify/4.1.0/ga-release/cloudify-enterprise-cli-4.1.rpm
 sleep 1
 
-cfy bootstrap /opt/cfy/cloudify-manager-blueprints/simple-manager-blueprint.yaml -i public_ip=$PUBLIC_IP -i ssh_user=cfyuser -i ssh_key_filename=/home/cfyuser/.ssh/key.pem -i ignore_bootstrap_validations=false -i admin_username=admin -i admin_password=admin
+cfy bootstrap /opt/cfy/cloudify-manager-blueprints/simple-manager-blueprint.yaml -i public_ip=$PUBLIC_IP -i private_ip=$PRIVATE_IP -i ssh_user=cfyuser -i ssh_key_filename=/home/cfyuser/.ssh/key.pem -i ignore_bootstrap_validations=false -i admin_username=admin -i admin_password=admin
 
 until cfy status;
     do sleep 3;
@@ -27,6 +28,7 @@ done
 sleep 15
 
 run_until_success "cfy profiles use -u admin -p admin -t default_tenant $PUBLIC_IP",
+
 #run_until_success "cfy secrets create ubuntu_trusty_image -s $UBUNTU_TRUSTY_IMAGE"
 #run_until_success "cfy secrets create centos_core_image -s $CENTOS_CORE_IMAGE"
 #run_until_success "cfy secrets create ubuntu_trusty_image -s $UBUNTU_TRUSTY_IMAGE"
